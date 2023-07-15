a71ff5f4a3ddf1810e08aa0795ed46a09b45c956
Mandy Jiang <mandyjiang2004@gmail.com>
2023-07-14 20:57:42 -0700

    90 degree pdf

--- app.py
+++ app.py
@@ -1,31 +1,45 @@
 from flask import Flask, render_template, request, send_file
+import PyPDF2
+import io
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
     # Handle file upload and text editing logic
-    uploaded_file = request.files['pdf']
-    # Process and edit the text in the PDF using PyPDF2 or other libraries
-    edited_pdf = process_pdf(uploaded_file)
-    # Save the edited PDF temporarily or generate it dynamically
-    # Provide a download link to the edited PDF in the result.html template
-    return render_template('result.html')
+    uploaded_file = request.files['pdfFile']
+    edited_pdf_io = process_pdf(uploaded_file)
 
-@app.route('/download')
-def download():
-    # Provide the edited PDF file for download
-    # Set appropriate headers to prompt the browser to download the file
-    return send_file('path_to_edited_pdf', as_attachment=True)
+    # Return the edited PDF as a downloadable file
+    edited_pdf_io.seek(0)  # Reset the file pointer to the beginning
+    return send_file(
+        edited_pdf_io,
+        mimetype='application/pdf',
+        as_attachment=True,
+        download_name='edited_pdf.pdf'
+    )
 
 def process_pdf(file):
-    # Perform PDF text editing using a PDF manipulation library
-    # Return the edited PDF
-    pass
+    # Perform PDF text editing using PyPDF2
+    pdf_reader = PyPDF2.PdfReader(file)
+    pdf_writer = PyPDF2.PdfWriter()
+
+    # Modify the PDF text or perform other editing operations
+    # Example: Rotating each page by 90 degrees
+    for page in pdf_reader.pages:
+        page.rotate(90)
+        pdf_writer.add_page(page)
+
+    # Save the edited PDF to a BytesIO object
+    edited_pdf_io = io.BytesIO()
+    pdf_writer.write(edited_pdf_io)
+    edited_pdf_io.seek(0)
+
+    return edited_pdf_io
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
